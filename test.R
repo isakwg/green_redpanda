f370710b28acf3fcc0cc6207ac12a0b88542d313
isakwg <isak.gaasland@gmail.com>
2025-03-31 10:48:11 +0200

    added a comment to the plot

--- test.R
+++ test.R
@@ -1,3 +1,17 @@
-usethis::use_github()
-
 ojnij
+
+library(palmerpenguins)
+library(ggplot2)
+
+penguins
+
+## making a plot
+p1 <- ggplot(penguins, aes(x = species, fill = species)) +
+  geom_bar() +
+  theme_minimal() +
+  labs(title = "Penguin species count",
+       x = "Species",
+       y = "Count") +
+  theme(legend.position = "none")
+
+p1
